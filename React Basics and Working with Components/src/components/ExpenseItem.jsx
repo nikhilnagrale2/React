7a8dde2de29c42aa32f908112de8b16fdc205802
Nikhil Nagrale <nknagrale634@gmail.com>
2021-07-08 18:12:59 +0530

    working with javascipt

--- React Basics and Working with Components/src/components/ExpenseItem.jsx
+++ React Basics and Working with Components/src/components/ExpenseItem.jsx
@@ -1,12 +1,16 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem() {
+  const expenseDate = new Date(2021, 2, 28);
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 294.24;
+
   return (
     <div className="expense-item">
-      <div>Match 28th 2021</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$294.56</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );
