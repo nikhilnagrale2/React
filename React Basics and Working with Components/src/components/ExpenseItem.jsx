3dd9af03f51904ac36cf1416b05490766dbb66bb
Nikhil Nagrale <nknagrale634@gmail.com>
2021-07-08 19:59:38 +0530

    refractoring expense date splitting to another component

--- React Basics and Working with Components/src/components/ExpenseItem.jsx
+++ React Basics and Working with Components/src/components/ExpenseItem.jsx
@@ -1,20 +1,12 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-  const expenseDate = props.date;
   const expenseTitle = props.title;
   const expenseAmount = props.amount;
-  const month = expenseDate.toLocaleString("en-US", { month: "long" });
-  const day = expenseDate.toLocaleString("en-US", { day: "2-digit" });
-  const year = expenseDate.getFullYear();
-
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+      <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
         <h2>{expenseTitle}</h2>
         <div className="expense-item__price">${expenseAmount}</div>
