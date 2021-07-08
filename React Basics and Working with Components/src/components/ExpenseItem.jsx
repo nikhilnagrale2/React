16881401ad00a0b8c4a848dd6c7a2dfdb64392f4
Nikhil Nagrale <nknagrale634@gmail.com>
2021-07-08 20:51:37 +0530

    Expense Item JX

--- React Basics and Working with Components/src/components/ExpenseItem.jsx
+++ React Basics and Working with Components/src/components/ExpenseItem.jsx
@@ -1,3 +1,4 @@
+import Card from "./Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
@@ -5,13 +6,13 @@ function ExpenseItem(props) {
   const expenseTitle = props.title;
   const expenseAmount = props.amount;
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
         <h2>{expenseTitle}</h2>
         <div className="expense-item__price">${expenseAmount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
