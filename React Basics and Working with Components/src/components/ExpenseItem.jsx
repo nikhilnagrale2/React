f6383115bd9aa6ab3d59480d0e4d2f2053adae83
Nikhil Nagrale <nknagrale634@gmail.com>
2021-07-08 18:35:30 +0530

    normal js added inside component

--- React Basics and Working with Components/src/components/ExpenseItem.jsx
+++ React Basics and Working with Components/src/components/ExpenseItem.jsx
@@ -4,10 +4,17 @@ function ExpenseItem(props) {
   const expenseDate = props.date;
   const expenseTitle = props.title;
   const expenseAmount = props.amount;
+  const month = expenseDate.toLocaleString("en-US", { month: "long" });
+  const day = expenseDate.toLocaleString("en-US", { day: "2-digit" });
+  const year = expenseDate.getFullYear();
 
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{expenseTitle}</h2>
         <div className="expense-item__price">${expenseAmount}</div>
